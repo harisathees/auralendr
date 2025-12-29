a3186281bef0d28867fb5fa07b33d839c7548c7b
harisathees <tsharisathees@gmail.com>
2025-12-29 22:43:07 +0530

    bug fixes

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,22 +1,35 @@
 <?php
 return [
+    //FOR LOCAL HOST
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    // 'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
+    // 'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        'http://localhost:5173',
-    ],
+    // 'allowed_origins' => [
+    //     'http://localhost:5173',
+    // ],
 
-    'allowed_origins_patterns' => [],
+    // 'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],
+    // 'allowed_headers' => ['*'],
 
+    // 'exposed_headers' => [],
+
+    // 'max_age' => 0,
+
+    // 'supports_credentials' => true,
+
+
+
+
+    //FOR PRODUCTION HOST
+    'paths' => ['api/*', 'login', 'logout', 'sanctum/csrf-cookie'],
+    'allowed_methods' => ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
+    'allowed_origins' => ['https://demo.auralendr.com'],
+    'allowed_headers' => ['Content-Type','Authorization','X-Requested-With','X-XSRF-TOKEN'],
     'exposed_headers' => [],
-
-    'max_age' => 0,
-
+    'max_age' => 600,
     'supports_credentials' => true,
-
 ];
+
