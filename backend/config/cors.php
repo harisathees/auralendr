6589b8a0f43aed38aee2a62b357f37594dadd914
harisathees <tsharisathees@gmail.com>
2025-12-29 22:43:07 +0530

    Add: cors for production level

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,12 +1,40 @@
 <?php
 return [
+    //FOR LOCAL HOST
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    // 'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    // 'allowed_methods' => ['*'],
+
+    // 'allowed_origins' => [
+    //     'http://localhost:5173',
+    // ],
+
+    // 'allowed_origins_patterns' => [],
+
+    // 'allowed_headers' => ['*'],
+
+    // 'exposed_headers' => [],
+
+    // 'max_age' => 0,
+
+    // 'supports_credentials' => true,
+
+
+
+
+    //FOR PRODUCTION HOST
+    'paths' => [
+        'api/*',
+        'login',
+        'logout',
+        'sanctum/csrf-cookie'
+    ],
 
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'http://localhost:5173',
+        'https://demo.auralendr.com',
     ],
 
     'allowed_origins_patterns' => [],
@@ -20,3 +48,4 @@ return [
     'supports_credentials' => true,
 
 ];
+
